aba74d9ff82640b366eff250eb62488c497684c3
jmlee0410 <dlwnsals83@gmail.com>
2021-05-15 20:26:54 +0900

    listitem 공통 컴포넌트 작업 전, css 반복 작업

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -11,19 +11,44 @@
       {{ user.title }}
     </div> -->
 
+    <ul class="news-list">
+      <li
+        v-for="item in this.$store.state.news"
+        :key="item.id"
+        class="post"
+      >
+        <div class="points">
+          {{ item.points }}  
+        </div>
 
+        <div>
+          <p class="news-title">
+            <a :href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
 
-    <p
+          <small class="link-text">
+            {{ item.time_ago }} by 
+            <router-link
+              class="link-text"
+              :to="`/user/${item.user}`"
+            >{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+
+    <!-- <p
       v-for="item in this.$store.state.news"
       :key="item.id"
     >
       <a :href="item.url"> {{ item.title }}</a>
       <small>
         {{ item.time_ago }} by 
-        <!-- <router-link v-bind:to="'/user/' + item.user">{{ item.user }}</router-link> -->
         <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
       </small>
-    </p>
+    </p> -->
   </div>
 </template>
 
@@ -57,6 +82,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282
+}
 </style>
